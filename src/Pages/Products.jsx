9cf9102cfda6b404b6142174a8baba8cee67b160
annisa rahmadani <annisarahmadani2311@gmail.com>
2025-07-09 21:16:47 +0700

    eventHandling

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -31,21 +31,39 @@ const products = [
   },
 ];
 
+const email = localStorage.getItem("email");
+
 const Products = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
   return (
-    <div className="flex justify-center py-5 gap-2">
-      {products.map((product) => {
-        return (
-          <CardProduct>
-            <CardProduct.Header url={product.image} />
-            <CardProduct.Body title={product.title}>
-              {product.deskripsi}
-            </CardProduct.Body>
-            <CardProduct.Footer harga={product.price} />
-          </CardProduct>
-        );
-      })}
-    </div>
+    <>
+      <div className="w-full h-[45px] bg-blue-600 flex justify-end items-center px-5">
+        <div className="text-lg text-white text-left">{email}</div>
+        <button
+          className="mx-2 bg-black text-white px-2 py-1 cursor-pointer"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+      <div className="flex justify-center py-5 gap-2">
+        {products.map((product) => {
+          return (
+            <CardProduct key={product.id}>
+              <CardProduct.Header url={product.image} />
+              <CardProduct.Body title={product.title}>
+                {product.deskripsi}
+              </CardProduct.Body>
+              <CardProduct.Footer harga={product.price} />
+            </CardProduct>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
